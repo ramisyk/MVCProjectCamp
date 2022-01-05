089c0bbd0ff65886f14e8f66efcb2a253e9fb71c
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-06 00:09:09 +0300

    Create some examples for views

---
+++ MVCProjectCamp/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Business.Concrete;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectCamp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager();
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var result = categoryManager.GetAll();
+            return View(result);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            categoryManager.Add(category);
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+}
