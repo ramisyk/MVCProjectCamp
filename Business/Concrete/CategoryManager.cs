d4e4e95b4cbd05ea1bcfd33979564bceb4e25837
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-05 20:40:35 +0300

    Category manager is greated
    manager objects are used for concrete business operations

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Concrete.Repositories;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAll()
+        {
+            return repo.GetAll();
+        }
+
+        public void Add(Category category)
+        {
+            if(category.CategoryName.Length < 4)
+            {
+                //Error Message
+            }
+            else
+            {
+                repo.Add(category);
+            }
+        }
+    }
+}
