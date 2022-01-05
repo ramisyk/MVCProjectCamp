dbaf18b9423d30268a26b5ba0176ecb349306a12
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-05 13:31:26 +0300

    repositories are created for crud operations

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IRepository<T>
+    {
+        //CRUD Operations
+        List<T> List();
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
---
+++ DataAccess/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context context = new Context();
+        DbSet<Category> _object;
+        public void Add(Category category)
+        {
+            _object.Add(category);
+            context.SaveChanges();
+        }
+
+        public void Delete(Category category)
+        {
+            _object.Remove(category);
+            context.SaveChanges();
+        }
+
+        public List<Category> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category category)
+        {
+            context.SaveChanges();
+        }
+    }
+}
