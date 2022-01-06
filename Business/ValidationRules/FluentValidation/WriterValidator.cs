edd88955517b93ed50f4920d8cdf0a4ba054a4c3
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-07 00:30:02 +0300

    add writer

---
+++ Business/ValidationRules/FluentValidation/WriterValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adı Boş Bırakılamaz.");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar Soyadı Boş Bırakılamaz.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında Boş Bırakılamaz");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girin");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen 50 karaktere kadar bir değer girin");
+            RuleFor(x => x.WriterAbout).Must(str => str.ToLower().Contains("a")).WithMessage("Yazar Hakkında Bilgisi 'a/A' Harfi İçermelidir."); 
+        }
+    }
+}
--- MVCProjectCamp/Controllers/WriterController.cs
+++ MVCProjectCamp/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
 using DataAccess.Concrete.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +19,28 @@ namespace MVCProjectCamp.Controllers
         {
             var result = writerManager.GetAll();
             return View(result);
+        }
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writer writer)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult validationResult = writerValidator.Validate(writer);
+
+            if (validationResult.IsValid)
+            {
+                writerManager.Add(writer);
+                return RedirectToAction("Index");
+            }
+            foreach (var item in validationResult.Errors)
+            {
+                ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+            }
+            return View();
         }
     }
 }
