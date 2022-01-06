1f5317439a20036d135e3a8d2170088f2702085d
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-06 23:53:55 +0300

    writer operations and screens are added

---
+++ MVCProjectCamp/Controllers/WriterController.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectCamp.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+        // GET: Writer
+        public ActionResult Index()
+        {
+            var result = writerManager.GetAll();
+            return View(result);
+        }
+    }
+}
