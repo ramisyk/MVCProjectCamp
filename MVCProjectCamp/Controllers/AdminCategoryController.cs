2f0df19d8362969768f6b0618ef4db5c0a57826b
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-06 21:54:43 +0300

    category controller migrated to admin catagory controller

--- MVCProjectCamp/Controllers/AdminCategoryController.cs
+++ MVCProjectCamp/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
+using DataAccess.Concrete.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +13,34 @@ namespace MVCProjectCamp.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
         // GET: AdminCategory
         public ActionResult Index()
         {
+            var result = categoryManager.GetAll();
+            return View(result);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            //categoryManager.Add(category);
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult validationResult = categoryValidator.Validate(category);
+            if (validationResult.IsValid)
+            {
+                categoryManager.Add(category);
+                return RedirectToAction("GetCategoryList");
+            }
+            foreach (var item in validationResult.Errors)
+            {
+                ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+            }
             return View();
         }
     }
