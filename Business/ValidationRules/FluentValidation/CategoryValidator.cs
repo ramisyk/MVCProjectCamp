bda7f2724c310415943212e9e0d6135619ed9d3a
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-06 20:18:37 +0300

    fluent validation is added

---
+++ Business/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adı Boş Bırakılamaz.");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama Boş Bırakılamaz");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girin");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Lütfen 20 karaktere kadar bir değer girin");
+        }
+    }
+}
