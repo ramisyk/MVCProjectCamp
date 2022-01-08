962f394c98d8a08f8e9809ad791b611b737d2af0
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-09 01:47:41 +0300

    add heading from ui

--- Business/ValidationRules/FluentValidation/WriterValidator.cs
+++ Business/ValidationRules/FluentValidation/WriterValidator.cs
@@ -17,7 +17,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında Boş Bırakılamaz");
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girin");
             RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen 50 karaktere kadar bir değer girin");
-            RuleFor(x => x.WriterAbout).Must(str => str.ToLower().Contains("a")).WithMessage("Yazar Hakkında Bilgisi 'a/A' Harfi İçermelidir."); 
+            //RuleFor(x => x.WriterAbout).Must(str => str.ToLower().Contains("a")).WithMessage("Yazar Hakkında Bilgisi 'a/A' Harfi İçermelidir."); 
         }
     }
 }
--- MVCProjectCamp/Controllers/HeadingController.cs
+++ MVCProjectCamp/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
+using Entity.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,42 @@ namespace MVCProjectCamp.Controllers
     {
         // GET: Heading
         HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
         public ActionResult Index()
         {
             var result = headingManager.GetAll();
             return View(result);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> categories = (from x in categoryManager.GetAll()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.CategoryName,
+                                                 Value = x.CategoryId.ToString()
+                                             }).ToList();
+
+            List<SelectListItem> writers = (from x in writerManager.GetAll()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.WriterName + " " + x.WriterSurname,
+                                                   Value = x.WriterId.ToString()
+                                               }).ToList();
+            ViewBag.categories = categories;
+            ViewBag.writers = writers;
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            headingManager.Add(heading);
+            return RedirectToAction("Index");
         }
     }
 }
