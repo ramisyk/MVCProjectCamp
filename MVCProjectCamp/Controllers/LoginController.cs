2c13794d107ed15886104c6a2257c00397c9933e
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-26 11:23:05 +0300

    login operation is created
    Login operation is created in controller. It will moved to backend architecture

--- MVCProjectCamp/Controllers/LoginController.cs
+++ MVCProjectCamp/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccess.Concrete;
+using Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,21 @@ namespace MVCProjectCamp.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            Context context = new Context();
+            var adminInfo = context.Admins.FirstOrDefault(x => x.AdminUserName == admin.AdminUserName);
+            if (adminInfo != null)
+            {
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            return RedirectToAction("Index");
         }
     }
 }
