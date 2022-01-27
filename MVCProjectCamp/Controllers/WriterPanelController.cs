e734d669a0c08c524d16d48268fe181c5111c8a6
Ramis Yüksel <ramisyk97@gmail.com>
2022-01-27 22:25:16 +0300

    writer panel controller and writer layout are created

---
+++ MVCProjectCamp/Controllers/WriterPanelController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectCamp.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        // GET: WriterPanel
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+        public ActionResult MyHeadings(int id=4)
+        {
+            var results = headingManager.GetAllByWriterId(id);
+            return View(results);
+        }
+    }
+}
